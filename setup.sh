43a98b35ef0ec7f9391adf24967a7d3d23549ba9
mitsu9 <mitsunobu.h.9@gmail.com>
2018-04-12 10:55:31 +0900

    Delete unnecessary files

--- setup.sh
+++ setup.sh
@@ -3,7 +3,5 @@
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/dotfiles/zsh/zshrc_local.sample ~/.zshrc_local
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/dotfiles/iterm2/ssh-background ~/bin/ssh-background
-chmod +x ~/bin/ssh-background
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/git/git_template ~/.git_template
