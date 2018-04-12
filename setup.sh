6cefae66e57af8376934a827821847a45ea509fe
mitsu9 <mitsunobu.h.9@gmail.com>
2018-04-12 10:51:44 +0900

    Change file name

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.zshrc_local ~/.zshrc_local
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/zshrc_local.sample ~/.zshrc_local
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/iterm2/ssh-background ~/bin/ssh-background
 chmod +x ~/bin/ssh-background
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/git/.git_template ~/.git_template
+ln -s ~/dotfiles/git/git_template ~/.git_template
