d8f8664c97439206a48a2d551eba940458e70f83
mitsu9 <mitsunobu.h.9@gmail.com>
2019-02-28 14:03:17 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ cp ~/dotfiles/zsh/zshrc_local.sample ~/.zshrc_local
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/git/git_template ~/.git_template
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
